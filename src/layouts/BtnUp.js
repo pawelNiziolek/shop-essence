7d415bf582c8f60f0a17db4723e2df90d01213c9
Paweł Niziołek <pawel.r.niziolek@gmail.com>
2019-07-11 15:28:41 +0200

    btnUp.js

--- src/layouts/BtnUp.js
+++ src/layouts/BtnUp.js
@@ -1,32 +1,24 @@
 import React from "react";
-import styled from "styled-components";
-import BackToTop from "react-back-to-top-button";
-
-const DivUp = styled.div`
-  position: fixed;
-  bottom: 60px;
-  right: 60px;
-  background-color: #ff084e;
-  border-radius: 0;
-  box-shadow: 0 2px 6px 0 rgba(0, 0, 0, 0.3);
-  color: #ffffff;
-  height: 40px;
-  width: 40px;
-  text-align: center;
-  z-index: 99090909;
-  cursor: pointer;
-`;
-
-const Iicon = styled.i`
-  font-size: 14px;
-  line-height: 40px;
-`;
+import { TinyButton as ScrollUpButton } from "react-scroll-up-button";
 
 const BtnUp = () => {
   return (
-    <BackToTop showOnScrollUp showAt={100} speed={1500} easing="easeInOutQuint">
-      <Iicon className="fas fa-chevron-up" aria-hidden="true" />
-    </BackToTop>
+    <ScrollUpButton
+      EasingType="easeInOutQuart"
+      AnimationDuration={900}
+      style={{
+        backgroundColor: "#ff084e",
+        padding: "0 12px",
+        fill: "white",
+        boxShadow: "0 2px 6px 0 rgba(0, 0, 0, 0.3)",
+        height: "40px",
+        width: "40px",
+        lineHeight: "45px",
+        marginRight: "30px",
+        bottom: "60px",
+        outline: "none"
+      }}
+    />
   );
 };
 
